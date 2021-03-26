6aa6ab84752bd85e4b0d629ca84a4dbe253a7ad8
Tomas <tomas.peschke@gmail.com>
2021-03-25 22:51:05 -0600

    added rule based move ordering

--- src/Gomoku3.py
+++ src/Gomoku3.py
@@ -65,5 +65,95 @@ def run():
     player=FlatMCSimPlayer(10,board)
     player.startSimulation('b')
 
+def rules(self, color, pos):
+    win = []
+    block = []
+    open_four = []
+    block_open_four = []
+    random = []
+
+    for move in self.board.get_empty_points:
+        for two_directions in [["N","S"], ["NE","SW"], ["E","W"], ["SE","NW"]]:
+            stats = self.line_rule(color, pos, two_directions)
+            if stats["win"]:
+                win.append(move)
+            elif stats["block"]:
+                block.append(move)
+            elif stats["open_four"]:
+                block.append(move)
+            elif stats["block_open_four"]:
+                block_open_four.append(move)
+            else:
+                random.append(move)
+
+    random.shuffle(win)
+    random.shuffle(block)
+    random.shuffle(open_four)
+    random.shuffle(block_open_four)
+    random.shuffle(random)
+
+    moves = win+block+open_four+block_open_four+random
+    if len(moves) == 0:
+        return "draw"
+    return moves[0]
+
+def line_rule(self, color, pos, two_directions):
+    l_mine = 0
+    l_theirs = 0
+    l_open = 0
+    r_mine = 0
+    r_theirs = 0
+    r_open = 0
+
+    increment = self.board.increments[two_directions[0]]
+    next_pos = pos+increment
+    pos_color = self.board.get_color(next_pos)
+    while (pos_color!=BORDER and l_open<=1):
+        if pos_color == color:
+            l_mine += 1
+            if l_theirs >= 1:
+                break
+        elif pos_color == EMPTY:
+            l_open += 1
+            break
+        else:
+            l_theirs += 1
+            if l_mine >= 1:
+                break
+
+    increment = self.board.increments[two_directions[1]]
+    next_pos = pos+increment
+    pos_color = self.board.get_color(next_pos)
+    while (pos_color!=BORDER and r_open<=1):
+        if pos_color == color:
+            r_mine += 1
+            if r_theirs >= 1:
+                break
+        elif pos_color == EMPTY:
+            r_open += 1
+            if r_mine+l_theirs >= 1:
+                break
+        else:
+            r_theirs += 1
+            if r_mine >= 1:
+                break
+
+    stats = {"win":False, "block_win":False, "open_four":False, "block_open_four":False}
+
+    if l_mine + r_mine >= 4:
+        stats["win"] = True
+        return stats
+    if l_theirs + r_theirs >= 4:
+        stats["block_win"] = True
+        return stats
+    if l_mine + r_mine == 3 and l_open + r_open == 2:
+        stats["open_four"] = True
+        return stats
+    if l_theirs + r_theirs == 3 and l_open + r_open == 2:
+        stats["block_open_four"] = True
+        return stats
+
+    return stats
+
 if __name__ == "__main__":
     run()
